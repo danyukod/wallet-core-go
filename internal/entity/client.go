6421771e399c989ee2b1e28b70befd27d30f7c99
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-08-30 23:23:00 -0300

    feat: add gitignore

---
+++ internal/entity/client.go
@@ -0,0 +1,24 @@
+package entity
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Client struct {
+	ID       string
+	Name     string
+	Email    string
+	CreateAt time.Time
+	UpdateAt time.Time
+}
+
+func NewClient(id, name, email string) (*Client, error) {
+	return &Client{
+		ID:       uuid.New().String(),
+		Name:     name,
+		Email:    email,
+		CreateAt: time.Now(),
+		UpdateAt: time.Now(),
+	}, nil
+}
