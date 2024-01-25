ece5a72762bdc03a8be4d39e7ad12af5be020973
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2024-01-25 18:44:07 -0300

    feat: create balance handle

---
+++ internal/event/handler/balance_updated_kafka.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/danyukod/chave-pix-utils/pkg/events"
+	"github.com/danyukod/chave-pix-utils/pkg/kafka"
+	"sync"
+)
+
+type UpdateBalanceKafkaHandler struct {
+	Kafka *kafka.Producer
+}
+
+func NewUpdateBalanceKafkaHandler(kafka *kafka.Producer) *UpdateBalanceKafkaHandler {
+	return &UpdateBalanceKafkaHandler{
+		Kafka: kafka,
+	}
+}
+
+func (h *UpdateBalanceKafkaHandler) Handle(message events.EventInterface, wg *sync.WaitGroup) {
+	defer wg.Done()
+	h.Kafka.Publish(message, nil, "balances")
+	fmt.Println("UpdateBalanceKafkaHandler called")
+}
